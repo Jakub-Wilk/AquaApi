70cdf5092606df710fc046cc07bf6093d924c3ec
Jakub Wilk <wilkjakub64@gmail.com>
2021-11-06 23:14:37 +0100

    Add basic JWT support

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
+import jwt
+import datetime
 from . import env
 
 app = Flask(__name__)
@@ -8,3 +10,23 @@ app.config["SECRET_KEY"] = env.secret_key
 
 mongo = PyMongo(app)
 
+def generate_jwt_token(username):
+    payload = {
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=5),
+        "iat": datetime.datetime.utcnow(),
+        "sub": username
+    }
+    return jwt.encode(
+        payload,
+        app.config.get("SECRET_KEY"),
+        algorithm="HS256"
+    )
+
+def validate_token(jwt_token):
+    try:
+        payload = jwt.decode(jwt_token, app.config.get("SECRET_KEY"))
+        return payload["sub"]
+    except jwt.ExpiredSignatureError:
+        return False
+    except jwt.InvalidTokenError:
+        return False
